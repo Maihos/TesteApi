50bc113d0fee09bbd7d38cb50329a87bc6ea9af6
Maihos <monteirojuninho@hotmail.com>
2025-05-09 15:47:21 -0300

    Att2 service

--- src/main/java/com/example/demo/Usuarios/Controller/UsuarioController.java
+++ src/main/java/com/example/demo/Usuarios/Controller/UsuarioController.java
@@ -1,21 +1,24 @@
 package com.example.demo.Usuarios.Controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Usuarios.Model.UsuarioModel;
 import com.example.demo.Usuarios.Service.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api")
 
 
 public class UsuarioController {
@@ -24,14 +27,18 @@ public class UsuarioController {
 
     @PostMapping("/cadastrar")
     public ResponseEntity <UsuarioModel> cadastrarUsuario (@Valid @RequestBody UsuarioModel usuarioModel) {
-        try{
-            UsuarioModel novoUsuarioModel = usuarioService.cadastrar(usuarioModel);
-            Map<String, Object> response = new HashMap<>();
-            response.put("mensagem", "Usuário cadastrado com sucesso.");
-            return new ResponseEntity<>(novoUsuarioModel, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        UsuarioModel novoUsuarioModel = usuarioService.cadastrar(usuarioModel);
+        return new ResponseEntity<>(novoUsuarioModel, HttpStatus.CREATED);
     }
 
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<UsuarioModel> atualizarUsuarioModel (@PathVariable Long id,@Valid @RequestBody UsuarioModel usuarioAtualizado) {
+        Optional<UsuarioModel> usuarioExistente = usuarioService.buscarPorId(id);
+
+        if (usuarioExistente.isPresent()){
+            usuarioAtualizado.setId(id);
+        }
+        
+        return;
+    }
 }
--- src/main/java/com/example/demo/Usuarios/Service/UsuarioService.java
+++ src/main/java/com/example/demo/Usuarios/Service/UsuarioService.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.Dto.UsuarioDto;
+import com.example.demo.Mapper.UsuarioMapper;
 import com.example.demo.Usuarios.Model.UsuarioModel;
 import com.example.demo.Usuarios.Repository.UsuarioRepository;
+
 import jakarta.validation.Valid;
 
 @Service
@@ -14,29 +18,40 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    private UsuarioMapper usuarioMapper;
+    
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioMapper = usuarioMapper;
+    }
 
-    public UsuarioModel cadastrar(@Valid UsuarioModel usuarioModel) {
-        if (usuarioRepository.existsByEmail(usuarioModel.getEmail())) {
+    public UsuarioDto cadastrar(@Valid UsuarioDto usuarioDto) {
+        UsuarioModel user = usuarioMapper.map(usuarioDto);
+        user = usuarioRepository.save(user);
+        if (usuarioRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("Email ja cadastrado");
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
-        usuarioModel.setSenha(usuarioModel.getDataNascimento().format(formatter));
-
-        return usuarioRepository.save(usuarioModel);
+        user.setSenha(user.getDataNascimento().format(formatter));
+        return usuarioMapper.map(user);
     }
 
     public Optional<UsuarioModel> buscarPorId(long id) {
         return usuarioRepository.findById(id);
     }
 
-    public UsuarioModel atualizar(UsuarioModel usuarioModel){
-        return usuarioRepository.save(usuarioModel);
+    public UsuarioDto atualizar(long id, UsuarioDto usuarioDto){
+        Optional<UsuarioModel> usuarioExistente = usuarioRepository.findById(id);
+        if (usuarioExistente.isPresent()){
+            UsuarioModel atualiza = usuarioMapper.map(usuarioDto);
+            atualiza.setId(id);
+            UsuarioModel salvado = usuarioRepository.save(atualiza);
+            return usuarioMapper.map(salvado); 
+        }
+        return null;
     }
 
     public void deletar(long id){
         usuarioRepository.deleteById(id);
     }
-
-
-
 }
