c5bcf2e9b9c555bd11b37e72332974c5bc6ebdeb
Maihos <monteirojuninho@hotmail.com>
2025-05-09 14:00:53 -0300

    att mensagem de cadastro

--- src/main/java/com/example/demo/Usuarios/Controller/UsuarioController.java
+++ src/main/java/com/example/demo/Usuarios/Controller/UsuarioController.java
@@ -1,19 +1,21 @@
 package com.example.demo.Usuarios.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Usuarios.Model.UsuarioModel;
 import com.example.demo.Usuarios.Service.UsuarioService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/usuarios")
 
 
 public class UsuarioController {
@@ -22,8 +24,14 @@ public class UsuarioController {
 
     @PostMapping("/cadastrar")
     public ResponseEntity <UsuarioModel> cadastrarUsuario (@Valid @RequestBody UsuarioModel usuarioModel) {
-        UsuarioModel novoUsuarioModel = usuarioService.cadastrar(usuarioModel);
-        return new ResponseEntity<>(novoUsuarioModel, HttpStatus.CREATED);
+        try{
+            UsuarioModel novoUsuarioModel = usuarioService.cadastrar(usuarioModel);
+            Map<String, Object> response = new HashMap<>();
+            response.put("mensagem", "Usuário cadastrado com sucesso.");
+            return new ResponseEntity<>(novoUsuarioModel, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
